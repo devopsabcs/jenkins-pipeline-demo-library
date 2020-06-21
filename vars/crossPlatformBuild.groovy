88ff4099f39a9ba602a76aba3ea0837200b4455e
emmanuelknafo <48259636+emmanuelknafo@users.noreply.github.com>
2020-06-21 10:50:30 -0400

    Update crossPlatformBuild.groovy

--- vars/crossPlatformBuild.groovy
+++ vars/crossPlatformBuild.groovy
@@ -20,7 +20,7 @@ def call(Map config) {
         environment {
             REPO_NAME = "${config.repoName}"
             DOCKER_LINUX_SERVER = 'dodockerlinux'            
-            DOCKER_WINDOWS_SERVER = 'dodockerwin2019'
+            DOCKER_WINDOWS_SERVER = 'dodockerwin19'
         }
         stages {
             stage('build') {
